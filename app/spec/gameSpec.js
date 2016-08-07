66be110625610b879c2f7caa2147646e9776fec8
Giancarlo <gfabrizi@gmail.com>
2016-08-07 17:54:49 +0200

    can roll a gutter & all ones game

--- app/spec/gameSpec.js
+++ app/spec/gameSpec.js
@@ -1,14 +1,20 @@
   describe('The BowlingGame', function() {
 
-    it('can create game', function() {
-      var game = new BowlingGame();
-    });
 
     it('can roll gutter game', function() {
       var game = new BowlingGame();
-      for ( i = 0; i < 20; i++) {
+      for ( var i = 0; i < 20; i++) {
         game.roll(0);
       }
       expect(game.score()).toBe(0);
+    });
+
+    it('can roll all ones', function() {
+      var game = new BowlingGame();
+      for ( var i = 0; i < 20; i++) {
+        game.roll(1);
+      }
+      expect(game.score()).toBe(20);
+
     });
   });
